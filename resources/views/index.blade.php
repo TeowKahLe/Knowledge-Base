36ec89dbdc73c5e586d2f1917a975d650aea72eb
App1e <teowkahle@gmail.com>
2024-11-15 14:54:02 +0800

    h

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer style="background-color: white; padding: 20px 0; position: fixed; bottom: 0; width: 100%; box-shadow: 0 -2px 4px rgba(0,0,0,.1);">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-4">
+                About Us
+            </div>
+            <div class="col-md-4">
+                <span style="padding: 0 10px; font-weight: bold;">•</span>
+                Contact Support
+                <span style="padding: 0 10px; font-weight: bold;">•</span>
+            </div>
+            <div class="col-md-4">
+                Privacy Policy
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
